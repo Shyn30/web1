85a88a9139e13c7476ae3e8e1c5ecf20e265ed9f
Shyn30 <sarjenfatih@gmail.com>
2023-12-04 00:04:29 +0700

    update produk

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
-// toggle class active
+// toggle class active humbergure menu
 // document query itu artinya carikan saya doc yg idnya
-const navbarNav = document.querySelector
-('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
-
-document.querySelector('#hamburger-menu').
-onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
+// toggle class active Searchform
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  // menambahkan kelas active ketika blm ada, ketika ada di klik maka hilang
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+
 // klik diluar side Bar untuk menuutup navbar dihamburger
-const hamburger = document.querySelector
-('#hamburger-menu');
-document.addEventListener('click',function(e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active')
-    };
-})
+const hamburger = document.querySelector("#hamburger-menu");
+const searchbutton = document.querySelector('#search-button');
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!searchbutton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
